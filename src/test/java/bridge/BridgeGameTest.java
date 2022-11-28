c127aaa3f4ba794353d26185aa696201a717b85a
parkmuhyeun <pjhg410@gmail.com>
2022-11-28 18:06:25 +0900

    test: BridgeGame 테스트 수정

--- src/test/java/bridge/BridgeGameTest.java
+++ src/test/java/bridge/BridgeGameTest.java
@@ -1,25 +1,44 @@
 package bridge;
 
+import bridge.model.Bridge;
 import bridge.model.User;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import java.util.ArrayList;
+import java.util.List;
 
 import static bridge.model.Command.RETRY;
 import static org.junit.jupiter.api.Assertions.*;
-import static org.mockito.ArgumentMatchers.any;
 
 class BridgeGameTest {
-    private static final BridgeGame bridgeGame = new BridgeGame();
+    private BridgeGame bridgeGame;
+
+    @BeforeEach
+    void setUp() {
+        bridgeGame = new BridgeGame();
+    }
 
     @Test
-    @DisplayName("사용자가 칸을 이동")
-    void move() {
+    @DisplayName("사용자가 칸을 이동 - Pass")
+    void moveWithPass() {
         User user = new User(new ArrayList<>());
-        bridgeGame.move(user, any(), any());
+        boolean isPass = bridgeGame.move(new Bridge(List.of("U", "D", "U")), "U", user);
 
+        assertTrue(isPass);
         assertEquals(1, user.getPath().size());
     }
+
+    @Test
+    @DisplayName("사용자가 칸을 이동 - Fail")
+    void moveWithFail() {
+        User user = new User(new ArrayList<>());
+        boolean isPass = bridgeGame.move(new Bridge(List.of("U", "D", "U")), "D", user);
+
+        assertFalse(isPass);
+        assertEquals(1, user.getPath().size());
+    }
+
 
     @Test
     @DisplayName("사용자가 게임을 다시 시도")
